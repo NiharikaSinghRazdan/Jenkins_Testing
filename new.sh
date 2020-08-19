8ffe0bbcab744bdb299e9527eff72cea266a617a
NiharikaSinghRazdan <niharika.singh.global@gmail.com>
2020-08-19 17:38:15 +0530

    included change

--- new.sh
+++ new.sh
@@ -2,5 +2,5 @@
 echo "Hi There"
 sudo apt update -y
 sudo apt install apache2 -y
-echo "Correct" > /var/www/html/index1.html
+echo "Connect" > /var/www/html/index1.html
 sudo service apache2 restart
